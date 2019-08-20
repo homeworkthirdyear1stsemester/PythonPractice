7ddbe841bc785cd106f6230eda0ac97c8b0c201e
kihwankim <clack2933@gmail.com>
2019-08-20 18:40:24 +0900

    draw something by using turtle

---
+++ practice13_graphic/practice02_draw.py
@@ -0,0 +1,29 @@
+import turtle
+
+for i in range(200):
+    turtle.fd(i)
+    turtle.left(360 / 4 + 10)
+
+
+# turtle.pencolor('green')
+#
+# for i in range(60):
+#     turtle.fd(50)
+#     turtle.left(360 / 3 + 10)
+#
+# turtle.pencolor('red')
+#
+# for i in range(60):
+#     turtle.fd(200)
+#     turtle.left(360 / 3 + 10)
+turtle.done()
+
+# turtle.color('red', 'yellow')
+#
+# for i in range(5 * 3):
+#     turtle.forward(100 + i * 10)
+#     turtle.right(360 / 5 * 2)
+#
+# turtle.end_fill()
+#
+# turtle.done()  # 실행하고 대기 한다
