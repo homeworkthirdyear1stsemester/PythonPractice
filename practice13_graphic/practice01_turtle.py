f2a7ce7a9bdd25c17c750c00eba97ea0b72aee00
kihwankim <clack2933@gmail.com>
2019-08-20 18:39:23 +0900

    turtle parctice

---
+++ practice13_graphic/practice01_turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.forward(100);
+turtle.right(90)
+turtle.forward(100)
+turtle.shape('turtle')
+turtle.color('red')
+turtle.color('red', 'yellow')
+turtle.hideturtle()
+turtle.showturtle()
