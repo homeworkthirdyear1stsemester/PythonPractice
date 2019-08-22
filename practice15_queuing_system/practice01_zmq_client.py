d8fe665d6b0b8f7b3cf8a444114ef740995f7d9c
kihwankim <clack2933@gmail.com>
2019-08-22 17:02:21 +0900

    pub and sub

--- practice15_queuing_system/practice01_zmq_client.py
+++ practice15_queuing_system/practice01_zmq_client.py
@@ -1,7 +1,8 @@
 import zmq
 
 context = zmq.Context()
-sock = context.socket(zmq.PULL)  # queue 에 존재 하지 않으므로 오류가 나지 않는다
+sock = context.socket(zmq.SUB)  # queue 에 존재 하지 않으므로 오류가 나지 않는다
+sock.setsockopt(zmq.SUBSCRIBE, b'sub1:')
 sock.connect("tcp://127.0.0.1:5690")
 
 while True:
--- practice15_queuing_system/practice01_zmq_server.py
+++ practice15_queuing_system/practice01_zmq_server.py
@@ -2,12 +2,12 @@ import time
 import zmq
 
 context = zmq.Context()
-sock = context.socket(zmq.PUSH)
+sock = context.socket(zmq.PUB)
 sock.bind("tcp://127.0.0.1:5690")
 
 id = 0
 while True:
     id += 1
-    sock.send(str(id).encode())
+    sock.send(('sub1 : ' + str(id)).encode())
     print("Sent : {}".format(id))
     time.sleep(1)
